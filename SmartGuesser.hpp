642fe921e025f1afdcdd0712df7cb1e0cc154202
ofrahadad <44833571+ofraHadad@users.noreply.github.com>
2019-05-05 19:15:07 +0300

    part 2 goodd

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -10,21 +10,25 @@ namespace bullpgia{
 	string gg="";
 	int index=0;
 	int index2=0;
+	int count=0;
+	string theNumbers="";
 	string guess() override;
 	void learn(string reply) override;
 	void startNewGame(int l) override{
+				wrong='9';
 				length=l;
 				gg="";
 				index=0;
 				index2=0;
 				count=0;
 				theNumbers="";
+				bull=0;
+				pgia=0;
 				for(int i=0; i<length;i++)
 					gg=gg+wrong;
 	
 		}
 
-	int count=0;
-	string theNumbers="";
+	
 	};
 };
